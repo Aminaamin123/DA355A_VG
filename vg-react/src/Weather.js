88d65e80842e1d925fb164f69968067468924181
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-25 17:43:33 +0200

    Adding useEffect and useState

--- vg-react/src/Weather.js
+++ vg-react/src/Weather.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Weather() {
 
     const url = "https://goweather.herokuapp.com/weather/Curitiba"
-    axios.get(url).then(response =>{
-    console.log(response.data)
-    })
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {
+        axios.get(url).then(response =>{
+            setWeather(response.data)
+            console.log(response.data)
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [url])
 
     return (
         <div>
+
+        {weather.temperature}
 
         </div>
     )
