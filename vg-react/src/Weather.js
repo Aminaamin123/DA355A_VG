799e421ce28a4411972736532fc7bbcf18f0595f
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-25 17:56:06 +0200

    Using date and date-fns to display date's.

--- vg-react/src/Weather.js
+++ vg-react/src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { addDays } from 'date-fns';
 
 export default function Weather() {
 
@@ -13,6 +14,10 @@ export default function Weather() {
         })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [url])
+
+    const date = new Date();
+    const tomorrow = addDays(date, 1);
+    const afterTomorrow = addDays(date, 2);
 
     return (
         <div>
