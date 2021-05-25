6b8134a0f93ed25ead0fa1cfe9f17f70bfd24dfc
Aminaamin123 <62305546+Aminaamin123@users.noreply.github.com>
2021-05-25 18:21:52 +0200

    feature, displaying image depending on the weather

--- vg-react/src/Image.js
+++ vg-react/src/Image.js
@@ -4,6 +4,9 @@ import { CloudyOutline } from 'react-ionicons';
 import { RainyOutline } from 'react-ionicons';
 
 export default function Image(props) {
+    console.log(props.item.description.includes("cloudy"));
+
+    if (props.item.description.includes("cloudy")){
         return (
             <div>
 
@@ -13,6 +16,13 @@ export default function Image(props) {
                 width="50px"
                 />
 
+            </div>
+        )
+    }
+    else if (props.item.description.includes("rain")){
+        return (
+            <div>
+
                 <RainyOutline
                 color={'#00000'}
                 shake
@@ -20,11 +30,19 @@ export default function Image(props) {
                 width="50px"
                 />
 
-              <SunnyOutline
-              color={'#00000'}
-              height="50px"
-              rotate
-              width="50px"
-              />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+
+            <SunnyOutline
+            color={'#00000'}
+            height="50px"
+            rotate
+            width="50px"
+            />
         </div>
     )
+}
